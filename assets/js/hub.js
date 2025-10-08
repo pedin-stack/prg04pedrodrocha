de75e2f50bebe891e041982256a173e418a6e88e
Pedro Dourado Rocha <pdourado2006@gmail.com>
2025-10-08 16:19:23 -0300

    feat(events & hub) : remoção da imagem de perfil também foi tentado deixar a side bar por padrão como : "colapsed"

--- assets/js/hub.js
+++ assets/js/hub.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const toggleBtn = document.getElementById('toggle-btn');
     const sidebar = document.querySelector('.sidebar');
 
+    sidebar.classList.add('collapsed');
+
     toggleBtn.addEventListener('click', function() {
         sidebar.classList.toggle('collapsed');
     });
